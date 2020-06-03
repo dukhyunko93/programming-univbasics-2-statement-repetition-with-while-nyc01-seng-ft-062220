9c1d95f3abcaef101ad45cc6bffe4dccbafa8420
Duke Ko <dukhyunko93@gmail.com>
2020-06-03 19:20:07 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
 count = 10
 while count >= 1 do
   puts count
   count -=1
 end
 puts "Happy New Year!"
+=======
+exit_number = 0
+count = 10
+while count > 0 && count != exit_number do
+  puts "#{count}"
+  if count == 0
+    puts "Happy New Year!"
+  end
+end
+>>>>>>> a943ab148eb88365aa02a15c2bb71722eb1a4cf0
